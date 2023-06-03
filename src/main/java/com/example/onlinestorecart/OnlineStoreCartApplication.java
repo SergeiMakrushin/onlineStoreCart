c1d1ae6a443e9ef779ada01dd901675b577ca912
Сергей <mak.sergei@ngs.ru>
2023-06-03 22:16:10 +0700

    One Commit

---
+++ src/main/java/com/example/onlinestorecart/OnlineStoreCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinestorecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinestorecart/OnlineStoreCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinestorecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
